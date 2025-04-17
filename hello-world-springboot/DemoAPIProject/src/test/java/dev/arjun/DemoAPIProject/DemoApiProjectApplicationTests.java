14fa3c9093ec75561e8df93e6fc3481c6ba5f95c
devx <devx@kali-mata>
2025-04-17 05:15:06 -0400

    hello-world-springboot-first-git-push

---
+++ hello-world-springboot/DemoAPIProject/src/test/java/dev/arjun/DemoAPIProject/DemoApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arjun.DemoAPIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
