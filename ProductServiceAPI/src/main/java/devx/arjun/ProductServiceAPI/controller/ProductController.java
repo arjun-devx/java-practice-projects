151b33821aa786dc2af0efeae5f8b19e3254649b
Arjun <arjun.devx@gmail.com>
2025-05-03 02:51:13 -0400

    Under ProductServiceApi project - Added Exception handling in controller

--- ProductServiceAPI/src/main/java/devx/arjun/ProductServiceAPI/controller/ProductController.java
+++ ProductServiceAPI/src/main/java/devx/arjun/ProductServiceAPI/controller/ProductController.java
@@ -4,6 +4,8 @@ import devx.arjun.ProductServiceAPI.dto.FakeStoreProductDTO;
 import devx.arjun.ProductServiceAPI.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpMessageConverterExtractor;
@@ -22,9 +24,13 @@ public class ProductController {
     }
 
     @GetMapping ("/product/{Id}")
-    public FakeStoreProductDTO getProduct(@PathVariable int Id) {
-
-        return productService.getProductFromFakeStore(Id);
+    public ResponseEntity<FakeStoreProductDTO> getProduct(@PathVariable("Id") int Id) {
+        if (Id <= 0) {
+            //throw new IllegalArgumentException("Product does not exists, provide valid productId");
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        FakeStoreProductDTO fakeStoreProductDTO = productService.getProductFromFakeStore(Id);
+        return new ResponseEntity<>(fakeStoreProductDTO, HttpStatus.OK);
     }
 
     @PostMapping ("/product")
@@ -33,9 +39,9 @@ public class ProductController {
         return productService.addProduct(fakeStoreProductDTO);
     }
 
-    @PutMapping ("/product/{id}")
-    public FakeStoreProductDTO updateProduct (@PathVariable int id, @RequestBody FakeStoreProductDTO fakeStoreProductDTO) {
-        return productService.updateProduct(id, fakeStoreProductDTO);
+    @PutMapping ("/product/{Id}")
+    public FakeStoreProductDTO updateProduct (@PathVariable("Id") int Id, @RequestBody FakeStoreProductDTO fakeStoreProductDTO) {
+        return productService.updateProduct(Id, fakeStoreProductDTO);
     }
 
     @DeleteMapping ("/product/{productId}")
