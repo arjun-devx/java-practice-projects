8d12cdaa0edaa071d54c1c20aade77b8714e0545
Arjun <arjun.devx@gmail.com>
2025-05-10 15:08:10 +0530

    ProductServiceAPI - added GlobalExceptionHandler class

---
+++ ProductServiceAPI/src/main/java/devx/arjun/ProductServiceAPI/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package devx.arjun.ProductServiceAPI.exception;
+
+import devx.arjun.ProductServiceAPI.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(ProductNotFoundException ex) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
+}
