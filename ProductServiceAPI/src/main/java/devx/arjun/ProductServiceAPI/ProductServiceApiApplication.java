ef297d0e62502451b856835b3eee100b6d32ebd0
Arjun <arjun.devx@gmail.com>
2025-06-15 16:33:16 +0530

    ThreadDemo - Added SemaPhore Package for Semaphore example - before implementing Semaphore trying to maintaing the thread of 5

--- ProductServiceAPI/src/main/java/devx/arjun/ProductServiceAPI/ProductServiceApiApplication.java
+++ ProductServiceAPI/src/main/java/devx/arjun/ProductServiceAPI/ProductServiceApiApplication.java
@@ -2,11 +2,13 @@ package devx.arjun.ProductServiceAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@RedisHash("admin")
 public class ProductServiceApiApplication {
 
 	public static void main(String[] args) {
